9b0b349111d620d18e7aff08bde9c3c1c7a56a8a
DamienCHAPEAU <damien.chapeau81@gmail.com>
2020-11-17 16:36:17 +0100

    Test login service

--- src/test/java/geco/LoginServiceTest.java
+++ src/test/java/geco/LoginServiceTest.java
@@ -1,4 +1,35 @@
 package geco;
 
-public class LoginServiceTest {
+import junit.framework.TestCase;
+import org.junit.Before;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LoginServiceTest extends TestCase {
+
+    private LoginService loginsExistants;
+
+    @Before
+    public void setUp() throws Exception {
+        loginsExistants = new LoginService(new String[]{"Login1","Login2"});
+    }
+
+    public void testLoginExists() {
+        assertTrue(loginsExistants.loginExists("Login1"));
+        assertFalse(loginsExistants.loginExists("Login6"));
+    }
+
+    public void testAddLogin() {
+        loginsExistants.addLogin("Login3");
+        assertTrue(loginsExistants.loginExists("Login3"));
+    }
+
+    public void testFindAllLoginsStartingWith() {
+        assertEquals(new ArrayList<String>(Arrays.asList(new String[]{"Login1", "Login2"})),loginsExistants.findAllLoginsStartingWith("Login"));
+    }
+
+    public void testFindAllLogins() {
+        assertEquals(new ArrayList<String>(Arrays.asList(new String[]{"Login1", "Login2"})),loginsExistants.findAllLogins());
+    }
 }
